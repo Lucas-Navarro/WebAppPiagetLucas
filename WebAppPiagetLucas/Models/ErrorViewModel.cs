42cf7a5e61a8388b7a058e7c3e790f7684a5fb96
Aluno <Aluno@UPGT-SPBR-PC165>
2025-04-29 10:06:23 -0300

    Adicionar arquivos de projeto.

---
+++ WebAppPiagetLucas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebAppPiagetLucas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
