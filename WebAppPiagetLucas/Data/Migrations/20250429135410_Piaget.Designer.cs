3c58f068ac55f74ba5a6a3ed59cba77aeeb353f1
Lucas-Navarro <lucasr6out@gmail.com>
2025-04-29 11:02:45 -0300

    Crud completos

--- WebAppPiagetLucas/Data/Migrations/20250429135410_Piaget.Designer.cs
+++ WebAppPiagetLucas/Data/Migrations/20250429135410_Piaget.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebAppPiagetLucas.Data;
 
@@ -11,9 +12,11 @@ using WebAppPiagetLucas.Data;
 namespace WebAppPiagetLucas.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250429135410_Piaget")]
+    partial class Piaget
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ WebAppPiagetLucas/Models/Base.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAppPiagetLucas.Models
+{
+    /// <summary>
+    /// Classe auxiliar
+    /// </summary>
+    public class Base
+    {
+        public Guid Id { get; set; }
+    }
+}
